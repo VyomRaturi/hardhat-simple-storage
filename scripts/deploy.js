019c423c08b25cff695e4380706bc37ad4bc1971
Vyom Raturi <vyomraturi2004@gmail.com>
2024-05-21 12:57:28 +0530

    deployed to sepolia , added custom tasks

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -17,45 +17,45 @@ async function main() {
   //______________________________________________
 
   // what happens when we deploy to our hardhat network?
-  // if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY) {
-  //   console.log("Waiting for block confirmations...")
+  if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY) {
+    console.log("Waiting for block confirmations...")
 
-  //   // Not functionable in version 6^ ethers ----->
+    // Not functionable in version 6^ ethers ----->
 
-  //   await simpleStorage.deploymentTransaction().wait(6)
-  //   await verify(simpleStorage.target, [])
+    await simpleStorage.deploymentTransaction().wait(6)
+    await verify(simpleStorage.target, [])
 
-  //   //______________________________________________
+    //______________________________________________
 
 
-  // }
+  }
 
-  // const currentValue = await simpleStorage.retrieve()
-  // console.log(`Current Value is: ${currentValue}`)
+  const currentValue = await simpleStorage.retrieve()
+  console.log(`Current Value is: ${currentValue}`)
 
   // Update the current value
-  // const transactionResponse = await simpleStorage.store(7)
-  // await transactionResponse.wait(1)
-  // const updatedValue = await simpleStorage.retrieve()
-  // console.log(`Updated Value is: ${updatedValue}`)
+  const transactionResponse = await simpleStorage.store(7)
+  await transactionResponse.wait(1)
+  const updatedValue = await simpleStorage.retrieve()
+  console.log(`Updated Value is: ${updatedValue}`)
 }
 
-// async function verify(contractAddress, args) {
-// const verify = async (contractAddress, args) => {
-//   console.log("Verifying contract...")
-//   try {
-//     await run("verify:verify", {
-//       address: contractAddress,
-//       constructorArguments: args,
-//     })
-//   } catch (e) {
-//     if (e.message.toLowerCase().includes("already verified")) {
-//       console.log("Already Verified!")
-//     } else {
-//       console.log(e)
-//     }
-//   }
-// }
+async function verify(contractAddress, args) {
+  // const verify = async (contractAddress, args) => {
+  console.log("Verifying contract...")
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    })
+  } catch (e) {
+    if (e.message.toLowerCase().includes("already verified")) {
+      console.log("Already Verified!")
+    } else {
+      console.log(e)
+    }
+  }
+}
 
 // main
 main()
